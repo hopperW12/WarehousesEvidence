3165e6900b88208eae5152db1dedad0d6da95520
hopperW12 <wolverinetomas@gmail.com>
2025-02-18 21:13:12 +0100

    Add AuditLogTable

---
+++ WarehousesEvidence.Web/Components/AuditLogTable/AuditLogTable.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using WarehousesEvidence.Data.Entities;
+using WarehousesEvidence.Data.Repositories;
+
+namespace WarehousesEvidence.Web.Components.AuditLogTable;
+
+public partial class AuditLogTable
+{
+    [Inject] 
+    public IAuditRepository _auditRepository { get; set; }
+    
+    private ICollection<AuditLog> Logs { get; set; } = new List<AuditLog>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Logs = await _auditRepository.GetAll();
+    }
+}
